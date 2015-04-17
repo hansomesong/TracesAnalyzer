08e5b4ab29dea9e646662fad5e97125aec692002
SeleneLI <yueli.cnfr@gmail.com>
2015-04-17 15:34:02 +0200

    Yue added FTT & autocorrelation

--- estimate_mr_coherence_2.py
+++ estimate_mr_coherence_2.py
@@ -161,24 +161,41 @@ if __name__ == '__main__':
     fft_size = 802
     x_axis = range(1, 803, 1) # 时间点
     means_fourier = np.fft.rfft(means)/fft_size # 对实数信号进行FFT计算, 为了正确显示波形能量，还需要将rfft函数的结果除以fft_size
-    freqs = np.linspace(0, sampling_rate/2, fft_size/2+1) # rfft函数的返回值是N/2+1个复数，分别表示从0(Hz)到sampling_rate/2(Hz)的N/2+1点频率的成分, 频域上的x轴
+    freqs = np.linspace(0, 1024, fft_size/2+1) # rfft函数的返回值是N/2+1个复数，分别表示从0(Hz)到sampling_rate/2(Hz)的N/2+1点频率的成分, 频域上的x轴
     # xfp = 20*np.log10(np.clip(np.abs(xf), 1e-20, 1e100)) # 计算每个频率分量的幅值，并通过 20*np.log10() 将其转换为以db单位的值。为了防止0幅值的成分造成log10无法计算，我们调用np.clip对xf的幅值进行上下限处理
     means_fp = np.abs(means_fourier)
 
 
-    pl.figure(figsize=(8,4))
-    pl.subplot(211)
-    pl.plot(x_axis, means)
-    pl.xlabel(u"时间(秒)")
-    pl.title(u"156.25Hz和234.375Hz的波形和频谱")
+    # pl.figure(figsize=(8,4))
+    # pl.subplot(211)
+    # pl.plot(x_axis, means, c="red")
+    # pl.xlim(0, 802)
+    # pl.xlabel("Experiment number")
+    # plt.ylabel("Mean of numbers of change")
+    # pl.title("Waveform and spectrum for numbers of change")
+    #
+    # pl.subplot(212)
+    # pl.plot(freqs, means_fp, c="blue")
+    # pl.xlim(0, 1024)
+    # pl.xlabel("Frequency (Hz)")
+    # pl.subplots_adjust(hspace=0.4)
+    # pl.show()
 
-    pl.subplot(212)
-    pl.plot(freqs, means_fp)
-    pl.xlim(-0.55, 0.55)
-    pl.xlabel(u"频率(Hz)")
-    pl.subplots_adjust(hspace=0.4)
-    pl.show()
+    # Autocorrelation
+    def autocorrelation(x,lags): #计算lags阶以内的自相关系数，返回lags个值，分别计算序列均值，标准差
+        n = len(x)
+        x = np.array(x)
+        result = [np.correlate(x[i:]-x[i:].mean(),x[:n-i]-x[:n-i].mean())[0]\
+            /(x[i:].std()*x[:n-i].std()*(n-i)) \
+            for i in range(1,lags+1)]
+        return result
 
+    correlation_result = autocorrelation(means, fft_size-2)
+    print "correlation_result:", correlation_result
+    plt.plot(np.linspace(1, 800, 800), correlation_result)
+    plt.xlabel("Order from 1 to (experiment number - 1)")
+    plt.ylabel("Range of Autocorrelation")
+    plt.title("Test of periodicity by using Autocorrelation")
 
 
 
@@ -201,7 +218,7 @@ if __name__ == '__main__':
     # plt.ylabel("Mean of numbers of change")
     # plt.title("Test of periodicity")
     # plt.grid()
-    # # plt.savefig("/Users/yueli/Documents/Codes/TracesAnalyzer/Plot/Plot_variable_time/Estimate_MR_Coherence_periodicity.pdf")
+    plt.savefig("/Users/yueli/Documents/Codes/TracesAnalyzer/Plot/Plot_variable_time/Estimate_MR_Coherence_periodicity_autocorrelation.pdf")
     plt.show()
 
 
