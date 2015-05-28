ac6e9c108161a0889d8d3acd84be899371cf0ecd
hansomesong <qpsong@gmail.com>
2015-05-28 09:27:42 +0200

    Modify plot_mapping_entry_grouper.py for merging the updates from Yue

--- Plot/Plot_mapping_entry_grouper.py
+++ Plot/Plot_mapping_entry_grouper.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from config.config import *
 import timeit
 import re
+import datetime
 
 # Import the targeted raw CSV file, and store them in the rawCSV_file_list
 rawCSV_file_list = []
@@ -14,13 +15,13 @@ rawCSV_file_list = []
 # rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-37.77.58.128-MR-149.20.48.61.log.csv"))
 # rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-37.77.59.0-MR-149.20.48.61.log.csv"))
 
-# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.216-MR-149.20.48.61.log.csv"))
-# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.217-MR-149.20.48.61.log.csv"))
-# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.218-MR-149.20.48.61.log.csv"))
-# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.220-MR-149.20.48.61.log.csv"))
+rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.216-MR-149.20.48.61.log.csv"))
+rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.217-MR-149.20.48.61.log.csv"))
+rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.218-MR-149.20.48.61.log.csv"))
+rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.22.220-MR-149.20.48.61.log.csv"))
 
-rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.17.224-MR-149.20.48.61.log.csv"))
-rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.17.228-MR-149.20.48.61.log.csv"))
+# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.17.224-MR-149.20.48.61.log.csv"))
+# rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.17.228-MR-149.20.48.61.log.csv"))
 
 # rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.30.160-MR-149.20.48.61.log.csv"))
 # rawCSV_file_list.append(os.path.join(PLANET_CSV_DIR, 'liege', "planetlab1-EID-153.16.30.164-MR-149.20.48.61.log.csv"))
@@ -90,6 +91,10 @@ if __name__ == '__main__':
     # 记录程序起始时间
     start_time = timeit.default_timer()
 
+    # All available marker styles are introduced in this webpage:
+    # http://matplotlib.org/api/markers_api.html
+    marker_styles = ['o', '*', '+', 'x', 's', 'q', '1', '2', '3', '4']
+
 
     # 画图时作为X轴的时间
     timeXAxis = getTime(rawCSV_file_list)
@@ -102,7 +107,27 @@ if __name__ == '__main__':
         i = i + 1
 
         plt.scatter(range(len(timeXAxis)), negativeValue_list, color = 'blue')
-        plt.scatter(range(len(timeXAxis)), normalValue_list, color = 'green')
+        plt.scatter(range(len(timeXAxis)), normalValue_list, color = 'green', marker='*')
+
+
+
+
+    for rawCSV_file in rawCSV_file_list:
+        with open(rawCSV_file) as f_handler:
+            next(f_handler)
+            for line in f_handler:
+                tmp_list = line.split(";")
+                # The data structure "LOG_COLUMN" is defined in file: config.py
+                if tmp_list[LOG_COLUMN['type']] != 'RoundNoReply':
+                    round_dict = {
+                        'type'      : tmp_list[LOG_COLUMN['type']],
+                        # Convert datetime of format string into a Datetime type object, and
+                        # gard just year, month, day, hour, minutes.
+                        'datetime'  : datetime.datetime.strptime(tmp_list[LOG_COLUMN['date']], "%Y-%m-%d %H:%M:%S")
+
+
+                    }
+
 
 
     plt.xlim(0, len(timeXAxis))
