e43c729597cb5e2f4fc289959937d9b178dc787b
hansomesong <qpsong@gmail.com>
2016-02-19 09:48:33 +0100

    Modify num_of_case_counter.py

--- 20160218Tasks/num_of_case_counter.py
+++ 20160218Tasks/num_of_case_counter.py
@@ -2,6 +2,7 @@
 __author__ = 'yueli'
 import numpy as np
 import matplotlib.pyplot as plt
+import pprint
 from config.config import *
 import datetime
 from collections import Counter
@@ -23,13 +24,34 @@ def num_case_counter(rawCSV_file):
     for line in open(rawCSV_file):
         lines = line.split(";")
         if lines[8] == "1":
-            dict_case_time['case1'].extend(i.split(" ")[0] for i in lines[15].split(","))
+            dict_case_time['case1'].extend([datetime.datetime.strptime(i.split(" ")[0], "%d/%m/%Y") for i in lines[15].split(",")])
         elif lines[8] == "3":
-            dict_case_time['case3'].extend(i.split(" ")[0] for i in lines[18].split(","))
+            dict_case_time['case3'].extend([datetime.datetime.strptime(i.split(" ")[0], "%d/%m/%Y") for i in lines[18].split(",")])
         elif lines[8] == "4":
-            dict_case_time['case4'].extend(i.split(" ")[0] for i in lines[18].split(","))
+            dict_case_time['case4'].extend([datetime.datetime.strptime(i.split(" ")[0], "%d/%m/%Y") for i in lines[18].split(",")])
 
     return dict_case_time
 
+def statiscs_process(input_dict):
+    for key, value in input_dict.iteritems():
+        input_dict[key] = sorted(Counter(value).most_common(), key=lambda x: x[0])
+        start_date = input_dict[key][0][0]
+        involved_dates = [x[0] for x in input_dict[key]]
+        for i in range(len(input_dict[key])):
+            start_date += datetime.timedelta(days=1)
+            if start_date not in involved_dates:
+                input_dict[key].append((start_date, 0))
+                print "Add datetime", start_date, "into current list"
+        input_dict[key] = sorted(Counter(input_dict[key]).most_common(), key=lambda x: x[0])
+
+
+
 if __name__ == "__main__":
-     print num_case_counter(rawCSV_file_liege)
+
+     result =  num_case_counter(rawCSV_file_liege)
+
+     statiscs_process(result)
+
+     pprint.pprint(result)
+
+
